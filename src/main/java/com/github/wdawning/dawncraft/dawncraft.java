b6fb694dd7a3f26832ac43f265c76e9a3833837d
Chen Wu <201436728@qq.com>
2016-08-21 20:25:43 +0800

    Update log and some other things.

--- src/main/java/com/github/wdawning/dawncraft/dawncraft.java
+++ src/main/java/com/github/wdawning/dawncraft/dawncraft.java
@@ -22,7 +22,7 @@ public class dawncraft
 {
     public static final String MODID = "dawncraft";
     public static final String NAME = "Dawn Craft Mod";
-    public static final String VERSION = "For mc1.8";
+    public static final String VERSION = "1.8";
 
     @Instance(dawncraft.MODID)
     public static dawncraft instance;
